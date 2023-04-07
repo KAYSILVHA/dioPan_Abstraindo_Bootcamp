47ac93cb2e5a0d3d5b4a6cf561a2de1411baab20
KAYSILVHA <kaysilvha@gmail.com>
2023-04-07 00:11:03 -0300

    final

--- desafio-dio-POO/src/Main.java
+++ desafio-dio-POO/src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -25,7 +27,37 @@ public class Main {
         System.out.println(curso2);
         System.out.println(mentoria);*/
 
+            Bootcamp bootcamp = new Bootcamp();
+            bootcamp.setNome("Bootcamp Java Developer");
+            bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+            bootcamp.getConteudos().add(curso1);
+            bootcamp.getConteudos().add(curso2);
+            bootcamp.getConteudos().add(mentoria);
 
+            Dev devKay = new Dev();
+            devKay.setNome("Kayra");
+            devKay.inscreverBootcamp(bootcamp);
+            System.out.println("Conteúdos Inscritos Kayra:" + devKay.getConteudosInscritos());
+            devKay.progredir();
+            devKay.progredir();
+            System.out.println("-");
+            System.out.println("Conteúdos Inscritos Kayra:" + devKay.getConteudosInscritos());
+            System.out.println("Conteúdos Concluídos Kayra:" + devKay.getConteudosConcluidos());
+            System.out.println("XP:" + devKay.calcularTotalXp());
+
+            System.out.println("-------");
+
+            Dev devRhay = new Dev();
+            devRhay.setNome("Joao");
+            devRhay.inscreverBootcamp(bootcamp);
+            System.out.println("Conteúdos Inscritos Rhayan:" + devRhay.getConteudosInscritos());
+            devRhay.progredir();
+            devRhay.progredir();
+            devRhay.progredir();
+            System.out.println("-");
+            System.out.println("Conteúdos Inscritos Rhayan:" + devRhay.getConteudosInscritos());
+            System.out.println("Conteúdos Concluidos Rhayan:" + devRhay.getConteudosConcluidos());
+            System.out.println("XP:" + devRhay.calcularTotalXp());
 
         }
 }
